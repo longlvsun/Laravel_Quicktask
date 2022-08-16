ae2602037318ddcc12e5e25d22ab51f3cc11c99e
longlvsun <le.van.long-b@sun-asterisk.com>
2022-08-17 02:12:48 +0700

    feat: add resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// method 1
+Route::resource('users', UserController::class);
+
+// method 2
+Route::prefix('notes')->name('notes.')->group(function() {
+    Route::get('', [NoteController::class, 'index'])->name('index');
+    Route::post('', [NoteController::class, 'store'])->name('store');
+    Route::get('create', [NoteController::class, 'create'])->name('create');
+    Route::get('{note}', [NoteController::class, 'show'])->name('show');
+    Route::match(['PUT', 'PATCH'], '{note}', [NoteController::class, 'update'])
+        ->name('update');
+    Route::delete('{note}', [NoteController::class, 'destroy'])->name('destroy');
+    Route::get('{note}/edit', [NoteController::class, 'edit'])->name('edit');
 });
